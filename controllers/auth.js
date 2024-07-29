0880194f8d0c45b6666970e726dab4b1b37dd2f7
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-07-29 10:20:28 -0700

    Add form post functionality and send response in corresponding route

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,12 @@
+const User = require('../models/user.js');
 const express = require('express');
 const router = express.Router();
 
-router.get('/sign-up', (req, res) => {
+router.get('/sign-up', async (req, res) => {
     res.render('auth/sign-up.ejs');
+});
+router.post('/sign-up', async (req, res) => {
+    res.send('form submission accepted');
 });
 
 module.exports = router;
