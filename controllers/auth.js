1f42e5f2cb009cf9397754418b6a393178a616b7
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-07-29 11:41:32 -0700

    Add sign out link, and route it to delete the session and afterwards redirect to index

--- controllers/auth.js
+++ controllers/auth.js
@@ -39,5 +39,9 @@ router.post('/sign-in', async (req, res) => {
     };
     res.redirect('/');
 });
+router.get('/sign-out', (req, res) => {
+    req.session.destroy();
+    res.redirect('/');
+});
 
 module.exports = router;
