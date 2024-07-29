e1a1114ca203ba437ea5150f5a7759130ebb5348
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-07-29 11:11:36 -0700

    Add logic to find user in database and determine if their credentials are correct

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const User = require('../models/user.js');
 const bcrypt = require('bcrypt');
 
-router.get('/sign-up', async (req, res) => {
+router.get('/sign-up', (req, res) => {
     res.render('auth/sign-up.ejs');
 });
 router.post('/sign-up', async (req, res) => {
@@ -21,6 +21,19 @@ router.post('/sign-up', async (req, res) => {
 });
 router.get('/sign-in', (req, res) => {
     res.render('auth/sign-in.ejs');
+});
+router.post('/sign-in', async (req, res) => {
+    const userInDatabase = await User.findOne({ username: req.body.username });
+    if (!userInDatabase) {
+        return res.send('Login failed. Please try again');
+    };
+    const validPassword = bcrypt.compareSync(
+        req.body.password,
+        userInDatabase.password,
+    );
+    if (!validPassword) {
+        return res.send("Login failed. Please try again");
+    };
 });
 
 module.exports = router;
