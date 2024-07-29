8de7d2e1a3b067f53f33c821e7478f2020bd9d4d
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-07-29 10:43:24 -0700

    Install bcrypt, add logic to enforce unique usernames, confirm password and confirm password fields are the same and encrypting the password before adding user to database

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,23 @@
-const User = require('../models/user.js');
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user.js');
+const bcrypt = require('bcrypt');
 
 router.get('/sign-up', async (req, res) => {
     res.render('auth/sign-up.ejs');
 });
 router.post('/sign-up', async (req, res) => {
-    res.send('form submission accepted');
+    const userInDatabase = await User.findOne({ username: req.body.username });
+    if (userInDatabase) {
+        return res.send("Username already taken.");
+    };
+    if (req.body.password !== req.body.confirmPassword) {
+        return res.send("Password and Confirm Password must match");
+    };
+    const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+    req.body.password = hashedPassword;
+    const user = await User.create(req.body);
+    res.send(`Thanks for signing up ${user.username}`);
 });
 
 module.exports = router;
