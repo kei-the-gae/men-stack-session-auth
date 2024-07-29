39a4b6ecc37ed69a22eeb1aca93fdf7166c07ac4
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-07-29 11:21:36 -0700

    Install express session, initialize in server

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
 const app = express();
-
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
+const session = require('express-session');
 
 // set port from environment variable or default to 3000
 const port = process.env.PORT ? process.env.PORT : '3000';
@@ -23,6 +23,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 // morgan for logging http requests
 app.use(morgan('dev'));
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true,
+    }),
+);
 
 app.get('/', async (req, res) => {
     res.render('index.ejs');
