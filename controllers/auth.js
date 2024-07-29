ea3ac75f25eef8c4b68b9bcbbeee787d011196db
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-07-29 09:53:40 -0700

    Change sign up render path so the sign up template renders

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/sign-up', (req, res) => {
-    res.render('sign-up.ejs');
+    res.render('auth/sign-up.ejs');
 });
 
 module.exports = router;
